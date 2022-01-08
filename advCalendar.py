2a5966c9096dae9e606af06a738a9368fa8a735b
Jia Qi Feng <jiaqifen@buffalo.edu>
2022-01-07 19:21:56 -0500

    day 2 of advent calendar done

--- advCalendar.py
+++ advCalendar.py
@@ -10,9 +10,31 @@ def countIncrease_day1():
         if prev != -1:
             if curr >= prev:
                 total += 1
-                prev = curr
         prev = curr
-    print(total)
+
+    return total
+
+def dive_day2():
+    horizontal = 0
+    depth = 0 
+    lines = []
+    with open('day2.txt') as f:
+        lines = f.read().splitlines()
+    
+    for i in lines:
+        move, amount = i.split(' ',1)
+        amount = int(amount)
+        if 'down' == move:
+            depth += amount
+        elif 'forward' == move:
+            horizontal += amount
+        elif 'up' == move:
+            depth -= amount
+        
+    total = depth * horizontal
+    return total 
 
 if __name__ == "__main__":
-    countIncrease_day1()
+    print(countIncrease_day1())
+    print(dive_day2())
+
